1ecf80c9cee389aef6bc14fa98859cf5d4318941
wornbb <wornbb@gmail.com>
2017-10-16 21:29:05 -0400

    First working prototype

--- Utility/Cleaner1.py
+++ Utility/Cleaner1.py
@@ -8,39 +8,58 @@ import pandas as pd
 from Utility.csv_reader import read_csv
 def cleaner1(mode,*files):
     del_items = []
-    [index,x] = read_csv(files[0])
-    try:
-        [index,y] = read_csv(files[1])
-        existence_of_y = 1
-    except:
-        existence_of_y = 0
-    k = np.nditer(x,flags=['f_index'])
-    while not k.finished:
-        entry = x[k.index]
-        entry = ''.join(filter(str.isalpha,entry))
-        if len(entry)>=5:
-            if not (('http' in entry) or ('php' in entry)):
-                x[k.index] = entry
+    if mode == 'train':
+        [index,x] = read_csv(files[0])
+        try:
+            [index,y] = read_csv(files[1])
+            existence_of_y = 1
+        except:
+            existence_of_y = 0
+        k = np.nditer(x,flags=['f_index'])
+        while not k.finished:
+            entry = x[k.index]
+            entry = ''.join(filter(str.isalpha,entry))
+            if len(entry)>=5:
+                if not (('http' in entry) or ('php' in entry)):
+                    x[k.index] = entry
+                else:
+                    del_items.append(k.index)
             else:
                 del_items.append(k.index)
-        else:
-            del_items.append(k.index)
-        k.iternext()
-    mask = np.ones(len(x),dtype=bool)
-    mask[del_items] = False
-    x = x[mask]
-    y = y[mask]
-    index = index[mask]
-    return [index,x,y]
-    #for pointer,entry,label in zip(index,x,y):
+            k.iternext()
+        mask = np.ones(len(x),dtype=bool)
+        mask[del_items] = False
+        x = x[mask]
+        y = y[mask]
+        index = index[mask]
+        return [index,x,y]
+    else:
+        x = np.array(files[0])
+        k = np.nditer(x, flags=['f_index'])
+        while not k.finished:
+            entry = x[k.index]
+            entry = ''.join(filter(str.isalpha, entry))
+            if len(entry) >= 5:
+                if not (('http' in entry) or ('php' in entry)):
+                    x[k.index] = entry
+                else:
+                    del_items.append(k.index)
+            else:
+                del_items.append(k.index)
+            k.iternext()
+        mask = np.ones(len(x), dtype=bool)
+        mask[del_items] = False
+        x = x[mask]
+        return [x]
 
-train_set_x = 'train_set_x.csv'
-train_set_y = 'train_set_y.csv'
-[index,x,y] = cleaner1("train",train_set_x,train_set_y)
-table = np.column_stack((x,y))
-df = pd.DataFrame(table)
-df.to_csv("test_utf8.csv",encoding='utf-8')
-#[index,x] = read_csv(train_set_x)
-#[index,y] = read_csv(train_set_y)
+    #for pointer,entry,label in zip(index,x,y):
+if __name__ == '__main__':
+    train_set_x = 'train_set_x.csv'
+    train_set_y = 'train_set_y.csv'
+    [index,x,y] = cleaner1("train",train_set_x,train_set_y)
+    table = np.column_stack((x,y))
+    df = pd.DataFrame(table)
+    df.to_csv("cleaner1.csv",encoding='utf-8')
+
 #table = np.column_stack((index,x,y))
 
--- bad_char_n_gram_baye/training.py
+++
@@ -1,5 +0,0 @@
-import sys
-from Utility.csv_reader import read_csv
-sys.path.append('..\Utility')
-[x,y] = read_csv('cleaner1.csv')
-
