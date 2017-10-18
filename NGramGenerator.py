9e69b73cb6af38f1893d610a641e1406b0582d44
Nicolas De Jay <ndj@pinkfilter.org>
2017-10-17 20:45:10 -0400

    Interface properly with scikit-learn.

--- NGramGenerator.py
+++ NGramGenerator.py
@@ -1,21 +1,3 @@
-# # Load data
-# 
-# from sklearn.pipeline import Pipeline, FeatureUnion
-# 
-# X  = pd.read_csv("data/train_set_x.csv")
-# Y  = pd.read_csv("data/train_set_y.csv")
-# X  = pd.merge(X, Y, on = 'Id')
-# Xn = pd.read_csv("data/test_set_x.csv")
-# 
-# # Split into n-grams
-# 
-# pipeline = Pipeline(
-#     [( 'ng', NGramGenerator() )] +
-# #   [( 'id', IdentityTransformer() )] +
-#     []
-# )
-# A = pipeline.fit_transform(X[:1000])
-
 import nltk as nltk
 import numpy as np
 import pandas as pd
@@ -28,14 +10,26 @@ from sklearn.base import TransformerMixin
 #   - multimap = [True, False]
 #   - low-count cutoff
 #   - 
+###################################################################################
+# Produce new representation of data such that each n-gram is associated with 
+# the number of times it is observed in a text of a particular language.
+#
+# I can think of two ways to encode using n-grams.
+#
+# Method #1: For each language, calculate the # of texts in which each n-gram
+#            has appeared.  This means that an n-gram is counted <once> per text.
+#
+# Method #2: For each language, calculate the # of occurrences of each n-gram across
+#            all texts.  This means that an n-gram is counted <multiple times> per text.
 
 class NGramGenerator(TransformerMixin):
-    def __init__(self):
-        None
+    def __init__(self, n=1, multimap=True, count_threshold=0, normalize=True, verbose=False):
+        self.n = n
+        self.multimap = multimap
+        self.count_threshold = count_threshold,
+        self.normalize = normalize
+        self.verbose = verbose
 
-        
-        
-    ###################################################################################
     # Extract n-grams from text snippit
     #
     # Input:
@@ -44,44 +38,13 @@ class NGramGenerator(TransformerMixin):
     # Output:
     #   - List<String> i.e. ["ok","ok"]
 
-    def string_to_ngrams(self, s, n = 1):
+    def string_to_ngrams(self, s, n=1):
         text = str(s).decode('utf-8').lower()
         text = text.replace(' ', '') # remove spaces
         ngrams = nltk.ngrams([c for c in text], n)
         return [''.join(g) for g in ngrams]
 
-    
-    
-    ###################################################################################
-    # Produce new representation of data such that each n-gram is associated with 
-    # the number of times it is observed in a text of a particular language.
-    #
-    # Input:
-    #   - pd.DataFrame train_set
-    #   - Boolean multimap: True if an n-gram is counted multiple times per text, and False
-    #                       if an n-gram is counted only once per text.
-    #   - Integer count_threshold:
-    #   - Boolean normalize
-    # Output:
-    #   - pd.DataFrame
-
-    # I can think of two ways to encode using n-grams.
-    #
-    # Method #1: For each language, calculate the # of texts in which each n-gram
-    #            has appeared.  This means that an n-gram is counted <once> per text.
-    #
-    # Method #2: For each language, calculate the # of occurrences of each n-gram across
-    #            all texts.  This means that an n-gram is counted <multiple times> per text.
-
-    def transform(self,
-                  X,
-                  Y = None,
-                  multimap = True,
-                  n = 1,
-                  verbose = False,
-                  count_threshold = 0,
-                  normalize = False):
-        
+    def transform(self, X, Y=None):
         Z = {}
         # Construct hash of arrays.
         for index, row in X.iterrows():
@@ -90,7 +53,7 @@ class NGramGenerator(TransformerMixin):
             category[row['Category']] = 1
             # Break the text into n-grams
             ngrams = self.string_to_ngrams(row['Text'], n = 1)
-            if not multimap:
+            if not self.multimap:
                 ngrams = list(set(ngrams))
             for ngram in ngrams:
                 if ngram in Z:
@@ -98,21 +61,28 @@ class NGramGenerator(TransformerMixin):
                     Z[ngram] = Z[ngram] + category # for some reason += works by reference and glitches
                 else:
                     Z[ngram] = category
-                if verbose:
+                if self.verbose:
                     print("%s:%s" % (ngram, Z[ngram]))
         # Convert into data frame   
         Z = pd.DataFrame(Z).transpose()
         # Filter low counts
-        keep = Z.apply(lambda row: sum(row) >= count_threshold, axis = 1)
+        keep = Z.apply(lambda row: sum(row) >= self.count_threshold, axis = 1)
         Z = Z[keep == 1]
         # Normalize by sum of y for each col y in Y
-        if normalize and Y != None:
+        if self.normalize and Y != None:
             totals = coll.Counter(Y['Category'])
             for colname in totals:
                 Z[colname] = Z[colname].apply(lambda x: 1. * x / totals[colname])
         # Return
         return Z
-    
+
     def fit(self, *_):
         return self
 
+    def get_params(self, deep=True):
+        return {}
+
+    def set_params(self, **parameters):
+        for parameter, value in parameters.items():
+            self.setattr(parameter, value)
+        return self
